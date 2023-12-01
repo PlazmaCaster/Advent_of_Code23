fafda235206cb13237aea84083447a46ec42ba0c
Quinn Haro <plazmacaster@proton.me>
2023-12-01 00:16:30 -0800

    c++ day1A done

---
+++ c++/day1/partA.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+
+int main() {
+    std::ifstream infile("input.txt");  // Input file
+    char input = '\0';                  // Test each character
+    int first = 0;                      // First digit
+    int second = 0;                     // Second digit
+    int sum = 0;                        // Running sum
+    bool newLine = true;                // Determines start of line
+
+    // Is my own file open? (good habit)
+    if (infile.is_open()) {
+
+        // Check EACH character
+        while (infile.get(input)) {
+            // If its a digit do conversion
+            if (std::isdigit(input)) {
+                if (newLine) {
+                    first = input - '0';
+                    newLine = false;
+                }
+                second = input - '0';
+            }
+
+            // If its a new line, add to running sum
+            if (input == '\n') {
+                sum += (10 * first) + second;
+                newLine = true;
+            }
+        }
+    }
+    // Done :)
+    std::cout << sum << '\n';
+
+    return 0;
+}
